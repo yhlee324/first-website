8f02d8758abb4ccd4ce29a0c47d7ebe363802a16
yhlee324 <yang.hwa.lee.24@dartmouth.edu>
2024-04-26 00:58:03 -0400

    First draft of recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Recipes from './components/recipes/Recipes';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Recipes />
     </div>
   );
 }

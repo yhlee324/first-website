f4ccbffa824f4cb1270f3532b80ceebe7aa01103
yhlee324 <yang.hwa.lee.24@dartmouth.edu>
2024-05-01 12:58:59 -0400

    Checkpoint for interests (pre-card)

---
+++ src/components/goals/Goals.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+function Goals() {
+  return (
+    <div className="goals">
+      <div className="container">
+        <h2> Here are some of my goals</h2>
+        <div className="card-container">
+          <Card body className="card1">
+            <h3>What I want to do in Dali</h3>
+            <p>My short-term goal is to get a summer internship in web development.</p>
+          </Card>
+          <Card body className="card2">
+            <h3>Long-term Goal</h3>
+            <p>My long-term goal is to develop an app for classical music enthusiasts.</p>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Goals;
--- src/components/interests/Interests.jsx
+++ src/components/interests/Interests.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState } from 'react';
 import './InterestsStyles.css';
 import Carousel from 'react-bootstrap/Carousel';
+import { Card } from 'react-bootstrap';
 import Goldberg from '../../assets/goldberg.jpg';
+import Cat from '../../assets/cat.jpg';
 
 function Interests() {
   const [index, setIndex] = useState(0);
@@ -13,29 +16,37 @@ function Interests() {
   return (
     <div className="interests">
       <div className="container">
+        <h2>Here are some of my interests</h2>
+        <div className="card-container">
+          <Card body className="card1">
+            <h3>Classical Music</h3>
+            <p>I love listening to classical music, especially Bach's Goldberg Variations.</p>
+          </Card>
+        </div>
         <div className="carousel-container1">
-          <h2>Here are some of my interests</h2>
-          <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel activeIndex={index} onSelect={handleSelect} className="carousel">
             <Carousel.Item>
               <img src={Goldberg} alt="FirstSlide" />
               <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h3>Classical Music</h3>
+                <p>Bach's Goldberg Variations is my favorite!</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img src={Goldberg} alt="SecondSlide" />
               <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolorn sit amet, consectetur adipiscing elit.</p>
+                <h3>Philosophy</h3>
+                <p>
+                  My favorite philosopher is Wittgenstein.
+                </p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img src={Goldberg} alt="LastSLide" />
+              <img src={Cat} alt="LastSLide" />
               <Carousel.Caption>
-                <h3>Third slide label</h3>
+                <h3>My Cat</h3>
                 <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                  This is my cat, Ray.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
